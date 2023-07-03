9bd09ed27b75ef75b1c66cfa4ffd6a6d4c8db025
Vivek <mailvivekkv@gmail.com>
2023-07-03 22:23:24 +0530

    backend comit

--- index.js
+++ index.js
@@ -20,27 +20,10 @@ const commentRoutes = require("./routes/comments");
 const videoRoutes = require("./routes/videos");
 const cookieParser = require("cookie-parser");
 
-const BASE_URL = process.env.BASE_URL;
 
 dotenv.config();
 connectDB();
 const app = express();
-
-app.use("/images", express.static(path.join(__dirname, "/images")));
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
 
 app.use(cookieParser());
 
@@ -57,10 +40,28 @@ app.use("/api/blogusers", blogUserRoute);
 app.use("/api/blogposts", blogPostRoute);
 app.use("/api/blogpcat", blogCatRoute);
 
+// Deployment code for chat
+const __dirname1 = path.resolve();
+const pd = path.dirname(__dirname1);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(pd, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(pd, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// Deployment end
+
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
@@ -70,7 +71,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: `${BASE_URL}`,
+    origin: "http://localhost:3000",
   },
 });
 
@@ -106,3 +107,5 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
+
+// Deoployment
