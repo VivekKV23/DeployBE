4ae2cb18fdc82eabcd240be7ca3a2b93b2756de1
Vivek-kv <74184283+VivekKV23@users.noreply.github.com>
2023-07-06 11:55:09 +0530

    local host  index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://workspace-jssstu-frontend.onrender.com",
+    origin: "http://localhost:3000",
   },
 });
 
